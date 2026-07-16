9e551679167e65985eec69c7e5273c4322568028
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-16 17:27:10 +0300

    Create welcome.sh

---
+++ installer/wizard/welcome.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+wizard_welcome() {
+
+    clear
+
+    cat <<EOF
+
+============================================================
+                 Lite Server Monitor
+                  Setup Wizard
+============================================================
+
+Welcome!
+
+This wizard will guide you through the installation
+of Lite Server Monitor.
+
+The installation usually takes less than five minutes.
+
+Press Enter to continue...
+
+EOF
+
+    read -r
+
+}
