fa5e3272d0a64652eed04c896368c9b52a00ec8e
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-16 09:25:38 +0300

    Update version.sh

--- commands/version.sh
+++ commands/version.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 command_version() {
+
     echo "${PROJECT_NAME}"
-    echo "Version: ${PROJECT_VERSION}"
+    echo "Version : ${PROJECT_VERSION}"
+    echo "Author  : ${PROJECT_AUTHOR}"
+    echo "License : MIT"
+
 }
