f98f7d2a7e66fc9fc08841cf9680fedf509ce798
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-17 14:17:30 +0300

    Update update.sh

--- installer/update.sh
+++ installer/update.sh
@@ -1,1 +1,25 @@
+#!/usr/bin/env bash
+#
+# -----------------------------------------------------------------------------
+# Lite Server Monitor (LSM)
+# Update Manager
+# -----------------------------------------------------------------------------
 
+set -Eeuo pipefail
+
+readonly LSM_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+main() {
+
+    echo "Updating Lite Server Monitor..."
+    echo
+
+    git -C "${LSM_ROOT}" pull
+
+    echo
+
+    "${LSM_ROOT}/installer/install.sh"
+
+}
+
+main "$@"
