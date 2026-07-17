966fa3c06b73473d59e66e199604b72133271af1
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-17 14:19:26 +0300

    Create install.sh

---
+++ modules/disk/install.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# -----------------------------------------------------------------------------
+# Lite Server Monitor (LSM)
+# Disk Module Installer
+# -----------------------------------------------------------------------------
+
+set -Eeuo pipefail
+
+MODULE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+log_info "Installing Disk module..."
+
+deploy_create_directory "/opt/lsm/modules/disk"
+
+deploy_install_file \
+    "${MODULE_DIR}/files/check_disk.sh" \
+    "/opt/lsm/modules/disk/check_disk.sh" \
+    755
+
+deploy_install_file \
+    "${MODULE_DIR}/files/lsm-disk.service" \
+    "/etc/systemd/system/lsm-disk.service"
+
+deploy_install_file \
+    "${MODULE_DIR}/files/lsm-disk.timer" \
+    "/etc/systemd/system/lsm-disk.timer"
+
+services_daemon_reload
+
+services_enable_and_start lsm-disk.timer
+
+log_success "Disk module installed."
