3d0801bf6b5c995e3b6edcdc23bac8ca22111760
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-19 23:21:07 +0300

    Update uninstall.sh

--- modules/disk/uninstall.sh
+++ modules/disk/uninstall.sh
@@ -7,13 +7,9 @@
 
 set -Eeuo pipefail
 
-services_stop_and_disable lsm-disk.timer
-
 deploy_remove_file /etc/systemd/system/lsm-disk.service
 deploy_remove_file /etc/systemd/system/lsm-disk.timer
 
 deploy_remove_directory /opt/lsm/modules/disk
-
-services_daemon_reload
 
 log_success "Disk module removed."
