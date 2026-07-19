4f591cdc3b8931cca7493037cf40ac8ebaa2cd62
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-19 23:30:48 +0300

    Update status.sh

--- commands/status.sh
+++ commands/status.sh
@@ -1,8 +1,43 @@
 #!/usr/bin/env bash
+#
+# -----------------------------------------------------------------------------
+# Lite Server Monitor (LSM)
+# Status Command
+# -----------------------------------------------------------------------------
 
-command_status() {
+set -Eeuo pipefail
 
-    log_info "Collecting system status..."
-    log_warn "Not implemented yet."
+echo
+echo "Lite Server Monitor"
+echo "==================="
+echo
 
-}
+echo "Version : $(cat /opt/lsm/VERSION 2>/dev/null || echo "Development")"
+echo
+
+echo "Installed Modules:"
+echo
+
+for dir in /opt/lsm/modules/*; do
+
+    [[ -d "${dir}" ]] || continue
+
+    printf "  • %s\n" "$(basename "${dir}")"
+
+done
+
+echo
+
+echo "Systemd Timers:"
+echo
+
+systemctl list-timers --all | grep "^lsm-" || true
+
+echo
+
+echo "Systemd Services:"
+echo
+
+systemctl --no-pager --type=service | grep "lsm-" || true
+
+echo
