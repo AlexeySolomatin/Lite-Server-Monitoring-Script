db4915b71b94a78d2cb658523abe269c5df3c35e
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-20 21:50:18 +0300

    Update email.sh

--- lib/ notifications/email.sh
+++ lib/ notifications/email.sh
@@ -1,1 +1,32 @@
+#!/usr/bin/env bash
+#
+# -----------------------------------------------------------------------------
+# Lite Server Monitor (LSM)
+# Email Notification
+# -----------------------------------------------------------------------------
 
+set -Eeuo pipefail
+
+
+CONFIG_FILE="/etc/lsm/config"
+
+
+[[ -f "${CONFIG_FILE}" ]] &&
+    source "${CONFIG_FILE}"
+
+
+
+TITLE="$1"
+MESSAGE="$2"
+
+
+
+if [[ -z "${EMAIL_TO:-}" ]]; then
+    exit 0
+fi
+
+
+echo "${MESSAGE}" |
+mail \
+    -s "LSM: ${TITLE}" \
+    "${EMAIL_TO}"
