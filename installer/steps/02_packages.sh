62d03d7861abce07520ea2331a5055c2f5db1e89
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-22 15:11:36 +0300

    Update 02_packages.sh

--- installer/steps/02_packages.sh
+++ installer/steps/02_packages.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
 step_packages() {
+    log_info "Installing required packages..."
 
-    log_step "Installing required packages"
+    update_package_cache
 
-    packages_update_cache
-
-    packages_install \
-        curl \
-        wget \
-        jq \
-        bc \
-        msmtp \
-        smartmontools \
-        mdadm \
-        lm-sensors \
+    local packages=(
+        curl
+        wget
+        jq
+        bc
+        msmtp
+        smartmontools
+        mdadm
+        lm-sensors
         fail2ban
+    )
 
+    for pkg in "${packages[@]}"; do
+        install_package "${pkg}"
+    done
 }
