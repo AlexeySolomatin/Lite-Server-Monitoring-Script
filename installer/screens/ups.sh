b713f663f965c32c5a5da477d522fe40d16ab7e7
AlexeySolomatin <74787973+AlexeySolomatin@users.noreply.github.com>
2026-07-23 13:01:56 +0300

    Update ups.sh

--- installer/screens/ups.sh
+++ installer/screens/ups.sh
@@ -6,48 +6,50 @@
 # UPS Configuration Screen
 # -----------------------------------------------------------------------------
 
+set -Eeuo pipefail
+
 INSTALL_UPS=false
 UPS_PROFILE=""
 
 screen_ups() {
-
     wizard_header
 
-    if ! wizard_yes_no "Install APC UPS support?"; then
-        return
+    echo -e "${CLR_BOLD}Настройка мониторинга ИБП (APC UPS):${CLR_RESET}"
+    echo "Позволяет отслеживать уровень заряда, напряжение и статус питания от батареи."
+    echo
+
+    if ! wizard_yes_no "Включить модуль мониторинга ИБП (APC UPS)?" "n"; then
+        INSTALL_UPS=false
+        UPS_PROFILE=""
+        return 0
     fi
 
     INSTALL_UPS=true
 
     echo
-    echo "Configuration"
-    echo
-    echo "1) APC Default"
-    echo "2) Configure later"
+    echo -e "  ${CLR_CYAN}1)${CLR_RESET} APC Стандартный профиль (apcupsd / localhost:3551)"
+    echo -e "  ${CLR_CYAN}2)${CLR_RESET} Настроить параметры позже"
     echo
 
     while true; do
-
-        read -rp "Select [1-2]: " answer
+        read -rp "$(echo -e "${CLR_BOLD}Выберите профиль [1-2]${CLR_RESET} [${CLR_YELLOW}1${CLR_RESET}]: ")" answer
+        answer="${answer:-1}"
 
         case "${answer}" in
-
             1)
-
                 UPS_PROFILE="default"
-
                 break
                 ;;
-
             2)
-
                 UPS_PROFILE="later"
-
                 break
                 ;;
-
+            *)
+                echo -e "${CLR_RED}Неверный выбор. Пожалуйста, введите 1 или 2.${CLR_RESET}"
+                ;;
         esac
-
     done
 
+    echo
+    echo -e "${CLR_GREEN}✓ Поддержка ИБП включена (${UPS_PROFILE}).${CLR_RESET}"
 }
